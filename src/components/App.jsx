da6e334cd2ce5f024ffd40731322fc9cadb66f27
Juan Vivanco <juansebastianvc@gmail.com>
2021-04-11 00:07:37 -0500

    Get images from unsplash api, console log only for test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,44 @@
 import React from "react";
+import './App.scss'
+import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-const App = () => {
+let Grid = () => {
+  // Hooks for images
+  const [images, setImages] = React.useState([]);
+
+  React.useEffect(() => {
+    fetchImages();
+  }, []);
+
+  // fetch images using any public API that returns photos(unsplash)
+  const fetchImages = (count = 10) => {
+    const apiRoot = "https://api.unsplash.com";
+    const accessKey =
+      "wyHH6b61LLtUgQmX93nPLSsrffoERj6gQ5jQiyGUwiA";
+
+    // random images
+    axios
+      .get(`${apiRoot}/photos/random?client_id=${accessKey}&count=${count}`)
+      .then(res => {
+        setImages([...images, ...res.data]);
+        setIsLoaded(true);
+
+        console.log(images);
+      });
+  };
+
   return (
-    <div>
-      <h1> Grid of photos </h1>
-    </div>
+    <div className="container">
+        {/* Some text */}
+        <h2>Reelgood front-end dev team assignment</h2>
+        <h1>
+          Grid of photos 
+        </h1>
+        <small>By <a href="https://juansvc.netlify.app" target="_blank">@juansvc</a></small>
+
+      </div>
   );
 };
 
-export default App;
+export default Grid;
